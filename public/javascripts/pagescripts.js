1930ede9f236bb2957bb01d6a5c486005860816d
Ted Meyer <tmathmeyer@gmail.com>
2014-02-22 00:43:34 -0500

    got a nice style on the landing page

---
+++ public/javascripts/pagescripts.js
@@ -0,0 +1,22 @@
+stations = [];
+
+load_stations = function(){
+	stations = ["abra", "absynth", "abacus", "abacore", "abstinance", "azeroth", "station1", "alsa", "pulseaudio", "fuck", "flubber", "slock", "slinkies"];
+}
+
+filter_stations = function(){
+	text = document.getElementById("inpute").value;
+	fun = stations.filter(function(data){
+		return data.indexOf(text.toLowerCase()) == 0;
+	});
+	render_grid(fun);
+}
+
+render_grid = function(stations){
+	table = "<ul id='searche'>";
+	stations.forEach(function(data){
+		table+="<li>"+data+"</li>";
+	});
+	table+="</ul>";
+	document.getElementById('table').innerHTML = table;
+}
