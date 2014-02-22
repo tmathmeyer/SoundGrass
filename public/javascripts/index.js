af4f5a24fb8ca1e1c2122ba52a48e73f1776451f
Patrick Plenefisch <simonpatp@gmail.com>
2014-02-22 02:41:38 -0500

    Adding play/pause states

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -11,13 +11,27 @@ function playable()
 								room_name: location.pathname});
 }
 
+function playClick()
+{
+	document.getElementById("play").style.display = "none";
+	document.getElementById("pause").style.display = "";
+	audio.volume = 1;
+}
+
+function pauseClick()
+{
+	document.getElementById("play").style.display = "";
+	document.getElementById("pause").style.display = "none";
+	audio.volume = 0;
+}
+
 window.addEventListener("DOMContentLoaded", function(){
 	var audio = document.querySelector("audio");
 	// testing
 	window.audio = audio;
 	audio.addEventListener("canplaythrough", playable);
 	socket = io.connect(location.origin);
-	
+
 	socket.on('connect', function () {
 		socket.emit('player join room', {'room_name': location.pathname});
 		playable();
@@ -36,4 +50,7 @@ window.addEventListener("DOMContentLoaded", function(){
 	socket.on('get station names', function(data){
 		console.log(data);
 	});
+	pauseClick();
+	document.getElementById("play").addEventListener("click", playClick);
+	document.getElementById("pause").addEventListener("click", pauseClick);
 });
